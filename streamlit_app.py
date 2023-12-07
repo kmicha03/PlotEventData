55cc2058bda3040fb70f2ded97317586372694fd
Konstantinos Michail <142662166+kmicha03@users.noreply.github.com>
2023-12-07 22:39:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -305,7 +305,7 @@ if create_plot_button:
     pitch_ax1.draw(ax=ax1)
 
     # Load the image
-    image_path = f"/Users/konstantinosmichail/Club Logos/{selected_team}_logo.png"
+    image_path = f"/Club Logos/{selected_team}_logo.png"
     image = plt.imread(image_path)
 
     imagebox = OffsetImage(image, zoom=0.15, resample=True, alpha=0.6)
