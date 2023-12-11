8c3269e4883eb35f94b9140734adcdb6b3112194
Konstantinos Michail <142662166+kmicha03@users.noreply.github.com>
2023-12-11 21:14:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -310,13 +310,13 @@ if len(selected_match_ids)>0:
       # Plot the completed passes
       pitch.lines(filtered_events[mask_complete].start_x, filtered_events[mask_complete].start_y,
                           filtered_events[mask_complete].end_x, filtered_events[mask_complete].end_y,
-                          lw=2, transparent=True, comet=True, label=f'Successful {event_type_correct_name}',
+                          lw=2.5, transparent=True, comet=True, label=f'Successful {event_type_correct_name}',
                           color=colour_success, ax=axs['pitch'])
     
         # Plot the other passes
       pitch.lines(filtered_events[~mask_complete].start_x, filtered_events[~mask_complete].start_y,
                           filtered_events[~mask_complete].end_x, filtered_events[~mask_complete].end_y,
-                          lw=2, transparent=True, comet=True, label=f'Unsuccessful {event_type_correct_name}',
+                          lw=2.5, transparent=True, comet=True, label=f'Unsuccessful {event_type_correct_name}',
                           color=colour_fail, ax=axs['pitch'],alpha=0.7)
       
       pitch.scatter(filtered_events[mask_complete].end_x, filtered_events[mask_complete].end_y,
@@ -330,11 +330,11 @@ if len(selected_match_ids)>0:
     
       pitch.lines(filtered_events[mask_complete].start_x, filtered_events[mask_complete].start_y,
                           filtered_events[mask_complete].end_x, filtered_events[mask_complete].end_y,
-                          lw=2, transparent=True, comet=True, label=f'Successful {event_type_correct_name}',
+                          lw=2.5, transparent=True, comet=True, label=f'Successful {event_type_correct_name}',
                           color=colour_success, ax=axs['pitch'])
       pitch.lines(filtered_events[~mask_complete].start_x, filtered_events[~mask_complete].start_y,
                           filtered_events[~mask_complete].end_x, filtered_events[~mask_complete].end_y,
-                          lw=2, transparent=True, comet=True, label=f'Unsuccessful {event_type_correct_name}',
+                          lw=2.5, transparent=True, comet=True, label=f'Unsuccessful {event_type_correct_name}',
                           color=colour_fail, ax=axs['pitch'],alpha=0.7)
       
       pitch.scatter(filtered_events[mask_complete].end_x, filtered_events[mask_complete].end_y,
