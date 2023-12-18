6b077ccc97cadc719d5d7209aee9a976ebb59164
Konstantinos Michail <142662166+kmicha03@users.noreply.github.com>
2023-12-18 15:48:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -361,7 +361,7 @@ if len(selected_match_ids)>0:
         axs['pitch'].text(position[0], position[1], text, ha='center', va='center', color = '#dee6ea')
 
     cmap = mpl.colormaps['RdYlGn']
-    colors = np.array([cmap(x) if x != 0 else (1, 0, 0, 1) for x in filtered_events['expectedGoalsOnTarget']])
+    colors = np.array([cmap(x) if x != 0 else '#86000a' for x in filtered_events['expectedGoalsOnTarget']])
 
     #plt.cm.get_cmap('RdYlGn')# Scale sizes based on expectedGoals
     pitch.lines(filtered_events[mask_complete].start_x, filtered_events[mask_complete].start_y,
